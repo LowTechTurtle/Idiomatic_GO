86e5695eea5a8cbd0a3ab6fc4482401d6474fb13
LowTechTurtle <dongquocthang1235@gmail.com>
2025-02-04 09:19:04 +0700

    update

---
+++ module_pkg_import/package_example/do-format/formatter.go
@@ -0,0 +1,7 @@
+package format
+
+import "fmt"
+
+func Number(i int) string {
+	return fmt.Sprintf("Da number is %d", i)
+}
---
+++ module_pkg_import/package_example/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/LowTechTurtle/Idiomatic_GO/tree/main/module_pkg_import/package_example/math"
+)
+
+func main() {
+	num := math.Double(1)
+	fmt.Println(format.Number(num))
+}
